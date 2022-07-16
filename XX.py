f2f4dfd8de70a50b15e541b013dd782104c27fed
Aymn-almsafr <86746382+Aymn-almsafr@users.noreply.github.com>
2022-07-16 12:08:12 +0200

    Update and rename XX.py to str.py

--- XX.py
+++
@@ -1,7 +0,0 @@
-
-import hashlib
-
-c="guwjshshsj"
-h = hashlib.sha256((c+',{.c][o^uecnlkijh*.iomv:QzCFRcd;drof/zx}w;ls.e85T^#ASwa?=(lk').encode()).hexdigest()
-h1=h.upper()
-print(h1)
---
+++ str.py
@@ -0,0 +1,6 @@
+
+import hashlib
+
+str=str,',{.c][o^uecnlkijh*.iomv:QzCFRcd;drof/zx}w;ls.e85T^#ASwa?=(lk'
+
+str=hashlib.sha256(str.encode()).hexdigest().upper()
